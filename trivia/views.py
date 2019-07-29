9199ab1421cf906ce2090a62646bc2c9379d1835
SLowrance <steve.lowrance@aronsonsecurity.com>
2019-07-29 10:48:01 -0700

    some basic styling and now have correct answer highlighted. Still need to work on JS to have check answer button turn on the correct answer highlighting.

--- trivia/views.py
+++ trivia/views.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 from trivia.sandbox.opentriviadb import get_questions
 
 
-@view_config(route_name='home', renderer="templates/mytemplate.pt")
+@view_config(route_name='home', renderer="templates/trivia_home.pt")
 def my_view(request):
     questions = get_questions()
     return {'project': 'trivia', 'questions': questions}
